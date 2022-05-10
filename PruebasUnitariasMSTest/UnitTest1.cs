24d7205dc9785e2abfffed353b6e43cabb0336e9
MattMrngStar <92487466+MattMrngStar@users.noreply.github.com>
2022-05-10 18:01:36 -0500

    Add files via upload

---
+++ PruebasUnitariasMSTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitariasMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+
+        }
+    }
+}
